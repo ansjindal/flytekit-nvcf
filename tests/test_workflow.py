c7326c2062d6939226902140d589140df4cfb069
Mohak <mchadha@nvidia.com>
2025-03-10 17:46:14 +0000

    Update

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -5,18 +5,18 @@ from flytekit import workflow
 from flytekitplugins.nvcf import nvcf_task
 
 # Get API key and org name from environment variables
-api_key = os.environ.get("NVCF_API_KEY","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")
-org_name = os.environ.get("NGC_ORG","0530795645140221")
+api_key = os.environ.get("NVCF_API_KEY")
+org_name = os.environ.get("NGC_ORG")
 
 # Define an NVCF task
-my_task = nvcf_task(
-    name="my-nvcf-task",
-    container_image="nvcr.io/0530795645140221/tasks_sample:latest",
+my_task_gfn = nvcf_task(
+    name="my-nvcf-task-gfn",
+    container_image="nvcr.io/0551317393399504/tasks_sample:v1",
     gpu_specification={"gpu": "L40S", "instanceType": "gl40s_1.br25_2xlarge", "backend": "GFN"},
     container_args="python3 main.py",
     max_runtime_duration="PT1H",
     max_queued_duration="PT1H",
-    termination_grace_period_duration="PT1H",
+    termination_grace_period_duration="PT10M",
     result_handling_strategy="",
     results_location=f"{org_name}/task-results",
     api_key=api_key,
@@ -33,7 +33,7 @@ my_task = nvcf_task(
 # Define a workflow that uses the NVCF task
 @workflow
 def my_workflow():
-    result = my_task()
+    result = my_task_gfn()
     return result
 
 
